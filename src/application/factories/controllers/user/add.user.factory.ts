b6a1b21910568d2f3660d7e32659437c0c838eb9
Enzo Rossi <enzorossi97@hotmail.com>
2024-03-08 16:20:21 -0300

    feat: criando a função de adicionar usuário

--- src/application/factories/controllers/user/add.user.factory.ts
+++ src/application/factories/controllers/user/add.user.factory.ts
@@ -1,12 +1,14 @@
 import { type Controller } from '../../../../domain/protocols/controller.protocol'
+import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt.adapter'
 import { UserRepositoryTypeORM } from '../../../../infra/data/typeorm/repositories/user.repository'
 import { AddUserController } from '../../../controllers/user/add.user.controller'
 import { AddUserUseCase } from '../../../usecases/user/add.user.usecase'
 import { logControllerFactory } from '../log/add-error.log.factory'
 
 export const AddUserControllerFactory = (): Controller => {
+    const hasher = new BcryptAdapter()
     const userRepo = new UserRepositoryTypeORM()
-    const addUser = new AddUserUseCase(userRepo, userRepo)
+    const addUser = new AddUserUseCase(userRepo, userRepo, hasher)
     const addUserController = new AddUserController(addUser)
     return logControllerFactory(addUserController)
 }
